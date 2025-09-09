2b702aea160e52f74ad6e79751d0e6dbe6c51dd8
SaeedSLTM <saeed.sltm@gmail.com>
2025-09-09 11:20:02 -0400

    Update isc-text_to_yml.py

--- isc-text_to_yml.py
+++ isc-text_to_yml.py
@@ -7,24 +7,27 @@ you can download all stations info registered in ISC using:
 and run this script to convert them.
 '''
 
-data = {}
+data = []
+codes = []
 with open("station.txt") as f, open("ISC_20250909.yml", "w") as g:
     next(f)
     for l in f:
         code = l[4:9]
+        if code in codes:
+            continue
+        codes.append(code)
         lat = float(l[10:19]) if l[10:19].strip() else nan
         lon = float(l[20:29]) if l[20:29].strip() else nan
         elv = float(l[30:35]) if l[30:35].strip() else nan
         st = l[48:68] if l[48:68].strip() else nan
         et = l[74:94] if l[74:74].strip() else nan
-        d = {"starttime": st,
+        info = {"code": code,
+             "starttime": st,
              "endtime": et,
              "latitude": lat,
              "longitude": lon,
              "elevation": elv}
-        if code not in data:
-            data[code] = []
-            data[code].append(d)
+        data.append(info)
     dump(data, g, default_flow_style=False, sort_keys=False)
     
         
