1957edcdda372a16d6080c408256263330e862ef
saeed <saeed@pc53802.iiees.ac.ir>
2025-09-13 09:10:57 -0400

    isc bug removed

--- isc-text2yml.py
+++ isc-text2yml.py
@@ -1,5 +1,6 @@
 from numpy import nan
 from yaml import dump
+from datetime import datetime as dt
 
 '''
 you can download all stations info registered in ISC using:
@@ -9,14 +10,16 @@ and run this script to convert them.
 
 data = []
 codes = []
-with open("station.txt") as f, open("ISC_20250909.yml", "w") as g:
+now = dt.now()
+
+with open("station.txt") as f, open(f"ISC_{now.strftime('%Y%m%d')}.yml", "w") as g:
     next(f)
     for l in f:
         code = l[4:9]
         if code in codes:
             continue
         codes.append(code)
-        lat = float(l[10:19]) if l[10:19].strip() else nan
+        lat = float(l[10:19].strip()) if l[10:19].strip() else nan
         lon = float(l[20:29]) if l[20:29].strip() else nan
         elv = float(l[30:35]) if l[30:35].strip() else nan
         st = l[48:68] if l[48:68].strip() else nan
